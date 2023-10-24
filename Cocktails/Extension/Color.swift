69cf69eef7cb48fe4b092a99c4780953ea5ea40a
brsaca <brsaca@gmail.com>
2023-10-24 12:58:25 -0600

    style: add custom colors

---
+++ Cocktails/Extension/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Cocktails
+//
+//  Created by Brenda Saavedra Cantu on 24/10/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let cGreen = Color("cGreen")
+    static let cYellow = Color("cYellow")
+    static let cWhite = Color("cWhite")
+    static let cBlack = Color("cBlack")
+}
