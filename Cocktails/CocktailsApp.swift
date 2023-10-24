6862f0e20428ef953288ac95e3e68b89207f2c1b
brsaca <brsaca@gmail.com>
2023-10-24 12:38:59 -0600

    Initial Commit

---
+++ Cocktails/CocktailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsApp.swift
+//  Cocktails
+//
+//  Created by Brenda Saavedra Cantu on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CocktailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cocktails/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cocktails
+//
+//  Created by Brenda Saavedra Cantu on 24/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
