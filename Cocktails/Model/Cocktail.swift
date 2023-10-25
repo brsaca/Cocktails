fa391280de0398c54347002d225a5fdd8949bc97
brsaca <brsaca@gmail.com>
2023-10-25 11:48:22 -0600

    refactor: set optional the values so i can use the model in list and detail request

--- Cocktails/Model/Cocktail.swift
+++ Cocktails/Model/Cocktail.swift
@@ -7,18 +7,18 @@
 
 import Foundation
 
-struct Cocktail: Codable, Identifiable {
+struct Cocktail: Codable, Identifiable{
     let id: String
     let title: String
     let difficulty: Difficulty
     let image: URL
-    let portion: String
-    let time: String
-    let description: String
-    let ingredients: [String]
-    let method: [Method]
+    let portion: String?
+    let time: String?
+    let description: String?
+    let ingredients: [String]?
+    let method: [Method]?
     
     var simplifiedTime: String {
-        return time.replacingOccurrences(of: "Hands-on time ", with: "")
+        return time?.replacingOccurrences(of: "Hands-on time ", with: "") ?? ""
     }
 }
