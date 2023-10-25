5629854c12f3782dba5805abca392b4bb61d79af
brsaca <brsaca@gmail.com>
2023-10-25 11:49:09 -0600

    sytle: add infoContainer and change alignment of ZStack

--- Cocktails/View/CocktailDetailView.swift
+++ Cocktails/View/CocktailDetailView.swift
@@ -14,24 +14,30 @@ struct CocktailDetailView: View {
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            // Image
-            ZStack {
+            ZStack(alignment: .topLeading) {
+                // Image
                 KFImage(cocktail.image)
                     .resizable()
                     .scaledToFill()
                     .frame(width: .infinity, height: UIScreen.main.bounds.size.height * 0.4, alignment: .topLeading)
                 
+                // Like
                 HStack {
                     Spacer()
-                    LikeButton(action: {}, size: 50)
+                    LikeButton(action: {}, size: 70)
                 }
-                .padding(.top, -100)
+                .padding(.top, 60)
                 .padding(.trailing, 30)
+                
+                // Info Container
+                InfoContainer(cocktail: cocktail)
+                .offset(y: UIScreen.main.bounds.size.height * 0.5)
+                
+                // Cook Button
+                
             }
-            
-            // Info Container
         }
-        .ignoresSafeArea()
+        .ignoresSafeArea(.all)
     }
 }
 
---
+++ Cocktails/View/InfoContainer.swift
@@ -0,0 +1,125 @@
+//
+//  InfoContainer.swift
+//  Cocktails
+//
+//  Created by Brenda Saavedra Cantu on 25/10/23.
+//
+
+import SwiftUI
+
+struct InfoContainer: View {
+    // MARK: View Properties
+    let cocktail: Cocktail
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(cocktail.title)
+                .font(.neue(.bold, size: 34))
+            
+            Text(cocktail.difficulty.detail)
+                .foregroundStyle(Color.cGreen)
+                .font(.neue(.text, size: 18))
+            
+            rateContainer
+            
+            if let ingredients = cocktail.ingredients {
+                getIngredientsContainer(for: ingredients)
+            }
+            
+            if let methodSteps = cocktail.method {
+                getSteps(for: methodSteps)
+            }
+            
+            Spacer()
+        }
+        .padding(.top, 40)
+        .padding(.horizontal, 30)
+        .background(.white)
+        .cornerRadius(30)
+    }
+}
+
+extension InfoContainer {
+    enum RateType {
+        case time
+        case portion
+        
+        var image: String {
+            switch (self) {
+            case .time: "clock.fill"
+            case .portion: "fork.knife.circle.fill"
+            }
+        }
+        
+        var color: Color {
+            switch (self) {
+            case .time: Color.gray
+            case .portion: Color.cYellow
+            }
+        }
+    }
+    
+    var rateContainer: some View {
+        HStack(spacing: 40) {
+            getRateInfo(for: .time)
+            getRateInfo(for: .portion)
+            Spacer()
+        }
+        .padding(.vertical, 20)
+    }
+    
+    @ViewBuilder
+    func getRateInfo(for rate:RateType) -> some View {
+        HStack(spacing: 10) {
+            Image(systemName: rate.image)
+                .resizable()
+                .frame(width: 20, height: 20)
+                .foregroundStyle(rate.color)
+            
+            Text(rate == .time ? cocktail.simplifiedTime : cocktail.portion ?? "")
+                .font(.neue(.text, size: 14))
+        }
+    }
+    
+    @ViewBuilder
+    func getIngredientsContainer(for ingredients: [String]) -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Ingredients")
+                .font(.neue(.semibold, size: 18))
+            
+            ForEach(ingredients, id:\.self) { ingredient in
+                Text("- \(ingredient)")
+                    .font(.neue(.thin, size: 18))
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func getSteps(for method: [Method]) -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Method steps")
+                .font(.neue(.semibold, size: 18))
+            
+            if let step1 = method[0].step1 {
+                Text("- \(step1)")
+                    .font(.neue(.thin, size: 18))
+            }
+            
+            if let step2 = method[0].step2 {
+                Text("- \(step2)")
+                    .font(.neue(.thin, size: 18))
+            }
+            
+            if let step3 = method[0].step3 {
+                Text("- \(step3)")
+                    .font(.neue(.thin, size: 18))
+            }
+        }
+    }
+}
+
+// MARK: - Previews
+#Preview {
+    InfoContainer(cocktail: Cocktail.mock)
+        .background(Color.systemGray)
+}
