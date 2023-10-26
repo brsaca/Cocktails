b5ed3919de3913d130142c624a3dee5e8aa22168
brsaca <brsaca@gmail.com>
2023-10-25 18:48:02 -0600

    refactor: constructor and add function to fetchCocktails async

--- Cocktails/ModelLogic/ContentLogic.swift
+++ Cocktails/ModelLogic/ContentLogic.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+@Observable
+final class ContentLogic {
+    static let shared = ContentLogic()
+    
+    let connection: ConnectionInteractor
+    
+    var cocktails: [Cocktail] = []
+    
+    var showEmptyView = false
+    
+    var isLoading = false
+    
+    var messageError = ""
+    
+    init(connection: ConnectionInteractor = Connection()) {
+        self.connection = connection
+    }
+    
+    func fetchCocktails() async {
+        self.isLoading = true
+        
+        do {
+            self.cocktails = try await connection.loadCocktails()
+            self.isLoading = false
+        } catch {
+            self.messageError = error.localizedDescription
+            self.showEmptyView = true
+            self.isLoading = false
+        }
+    }
+}
